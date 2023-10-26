0d6bb4c91efacad827bb364052a3c9b0008960ed
vboxuser <vboxuser@ubuntalinux.myguest.virtualbox.org>
2023-10-26 18:08:51 +0500

    task1task2

--- labs/lab2/task1.c
+++ labs/lab2/task1.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
 	int i, n;
 	scanf("%d", &n);
 	int mas[n];
+	srand(time(NULL));
 	for (i = 0; i < n; i++) {
 		mas[i] = rand() % 100;
 		printf("%d ", mas[i]);
--- labs/lab2/task2.c
+++ labs/lab2/task2.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
 	FILE *file = fopen("task2.txt", "w");
 	int i,n;
 	scanf("%d", &n);
 	int mas[n];
+	srand(time(NULL));
 	for (i = 0; i < n; i++) {
 		mas[i] = rand() % 100;
 		printf("%d ", mas[i]);
